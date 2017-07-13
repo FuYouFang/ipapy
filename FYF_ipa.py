397abbe077f6bfa4370b192246bbab33fae952f0
fuyoufang <fuyoufang@163.com>
2017-07-13 14:38:05 +0800

    add some thing

--- FYF_ipa.py
+++ FYF_ipa.py
@@ -13,41 +13,30 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 from datetime import date, time, datetime, timedelta
+import FYF_ipa_config
+
 
 # 配置文件夹
 #commandPath = '/Users/' + getpass.getuser() + '/.ipa_build_py'
-commandPath = '/Users/xuekai/.ipa_build_py'
+commandPath = '/Users/xuekai/.fyf_ipa_build_py'
 # 配置文件名
-# configFileName = "ipaBuildPyConfigFile.json"
+configFileName = "ipaBuildPyConfigFile.json"
 # 配置文件路径
-# configFilePath = os.path.join(commandPath, configFileName)
+configFilePath = os.path.join(commandPath, configFileName)
 
-
-
-
+# 初始化配置文件
 def initConfigFile():
-    createConfigFile()
-    initConfigFileContent()
+    # 初始化配置文件夹
+    if not os.path.exists(commandPath):
+        try:
+            os.makedirs(commandPath)
+            return True
+        except OSError as error:
+            print('创建配置文件夹失败：%s' % error)
+            return False
 
 
-    # 参数设置
-def setOptparse():
-    parser = optparse.OptionParser()
-    # 参数配置指令
-    parser.add_option('-c', '--config', action='store_true', default=None, help='config User''s dat')
-    # 获取所有版本
-    parser.add_option('-s', '--showTags', action='strore_true', default=None, help='show all tags')
-    # 设置版本指令
-    parser.add_option('-t', '--tag', default='master', help='app''s tag')
-    options, arguments = parser.parse_args()
-    global tag
-    tag = options.tag
-    # 配置信息
-    if options.config == True and len(arguments) == 0:
-        configMethod()
-    # 获取所有版本
-    if options.showTags == True and len(arguments) == 0:
-        gitShowTags()
+
 
 def configMethod():
     return
@@ -55,10 +44,177 @@ def configMethod():
 def gitShowTags():
     return
 
+# 1.判断是否存在帮助、展示类的参数
+#   1.存在
+#       1.展示帮助信息
+#       2.展示其他参数
+#
+# 1.判断参数种是否存在 -c 或者 --config
+#   1. 存在
+#       1.判断本地是否保存有配置的 json
+#           1.存在
+#               1.检查本地的配置是否符合打包条件
+#                   1.符合打包条件，输出本地配置，并开始用本地的配置进行打包
+#                   2.不符合打包条件，
+#                       1.输出不符合的条件，并提示进行确认，
+#                       2.配置确认完成之后，对配置进行保存
+#                       3.开始进行打包
+#           2.不存在，逐条询问各个配置，询问完毕之后，进行打包
+#
+#   2. 不存在
+#       1.逐条询问各个配置，
+#       2.确定好配置后，保存配置
+#       3.根据配置进行打包
+
+class ipaTool(object):
+    parser = optparse.OptionParser()
+    options = None
+    arguments = None
+    config = None
+
+    def __init__(self):
+        self.setupParse();
+        self.projectPath = '/Users/xuekai/Documents/work/com/HappyFishing_iPhone_T'
+
+    # 参数设置
+    def setupParse(self):
+        # 参数配置指令
+        self.parser.add_option('-c', '--config', action='store_true', default=None, help='config User''s dat')
+        # 展示所有版本
+        self.parser.add_option('-s', '--showTags', action='store_true', default=None, help='show all tags')
+        # 设置版本指令
+        self.parser.add_option('-t', '--tag', default='master', help='app''s tag')
+
+        self.options, self.arguments = self.parser.parse_args();
+
+    # 根据配置打包
+    def packageWithConfig(self):
+        self.cleanProgect()
+        self.archiveProgect()
+        self.exportArchive()
+
+        #os.system('cd %s;xcodebuild -target %s clean' % (self.config.projectPath, self.config.targetName))
+
+        return
+
+    # 是否要根据配置打包
+    def isPackageWithConfigInParser(self):
+        return self.options.config == True
+
+    # 是否要展示配置
+    def isShowTagsInParser(self):
+        return self.options.showTags == True
+
+    # 展示 tags
+    def showTagsInParser(self):
+        print('展示tags')
+
+    def checkConfig(self):
+        return False
+
+    # 判断是否是workspace
+    def checkWorkSpace(self):
+        if os.path.exists("%s/%s.xcworkspace" % (self.config.projectPath, self.config.targetName)):
+            return True
+        else:
+            return False
+
+
+    def compliteConfig(self):
+        #taget = input('taget:')
+        if self.config == None:
+            self.config = FYF_ipa_config.IpaConfig()
+
+        # 配置参数
+        self.config.projectPath = '/Users/xuekai/Documents/work/com/HappyFishing_iPhone_T'
+        self.config.targetName = 'HappyFishing_iPhone'
+        self.config.archivePath = '/Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_archivePath/HappyFishing_iPhone.xcarchive'
+        self.config.exportArchivePath = '/Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_exportArchivePath'
+        self.config.exportOptionsPlistFilePath = '/Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_exportArchivePath/adhoc.plist'
+        self.config.configuration = 'Release'
+        # CODE_SIGN_IDENTITY = "iPhone Distribution: jianwei liu (F658SC654U)";
+        self.config.identity = 'iPhone Distribution: jianwei liu (F658SC654U)'
+        # PROVISIONING_PROFILE = "b2ec3855-d750-4e9c-a89a-3c8ced56c06d";
+        # PROVISIONING_PROFILE_SPECIFIER = shangYuDistribution;
+        self.config.profile = 'b2ec3855-d750-4e9c-a89a-3c8ced56c06d'
+        self.checkWorkSpace()
+
+    def beginWork(self):
+        if self.isShowTagsInParser():
+            self.showTagsInParser()
+
+        if self.isPackageWithConfigInParser():
+            if not self.checkConfig():
+                self.compliteConfig()
+            self.packageWithConfig()
+
+
+    # clean工程
+    def cleanProgect(self):
+        if self.checkWorkSpace():
+            # xcodebuild clean -configuration "Release" -alltargets
+            # os.system(
+            #     'cd %s;xcodebuild -workspace %s.xcworkspace -scheme %s clean' % (self.config.projectPath, self.config.targetName, self.config.targetName))
+            print('cd %s;xcodebuild -workspace %s.xcworkspace -scheme %s clean' % (self.config.projectPath, self.config.targetName, self.config.targetName))
+        else:
+            os.system('cd %s;xcodebuild -target %s clean' % (self.config.projectPath, self.config.targetName))
+        return
+
+    def archiveProgect(self):
+        if self.checkWorkSpace():
+            # os.system(
+            #     'cd %s;xcodebuild -workspace %s.xcworkspace -scheme %s -archivePath %s archive' % (self.config.projectPath, self.config.targetName, self.config.targetName, self.config.archivePath))
+            print('cd %s;xcodebuild archive -workspace %s.xcworkspace -scheme %s -configuration %s -archivePath %s' % (
+                self.config.projectPath,
+                self.config.targetName,
+                self.config.targetName,
+                self.config.configuration,
+                self.config.archivePath))
+        else:
+            os.system('cd %s;xcodebuild -target %s clean' % (self.config.projectPath, self.config.targetName))
+        return
+
+    # xcodebuild -exportArchive -archivePath /Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_archivePath/HappyFishing_iPhone.xcarchive -exportPath /Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_exportArchivePath -exportOptionsPlist /Users/xuekai/Documents/work/com/HappyFishing_iPhone_T_exportArchivePath/adhoc.plist CODE_SIGN_IDENTITY="iPhone Distribution: jianwei liu (F658SC654U)" PROVISIONING_PROFILE="b2ec3855-d750-4e9c-a89a-3c8ced56c06d"
+    def exportArchive(self):
+        if self.checkWorkSpace():
+            # os.system(
+            #     # 'cd %s;/'
+            #     'xcodebuild -exportArchive /'
+            #     '-archivePath %s  /'
+            #     '-exportPath %s  /'
+            #     '-exportOptionsPlist %s /'
+            #     'CODE_SIGN_IDENTITY=%s /'
+            #     'PROVISIONING_PROFILE=%s' % (
+            #         # self.config.projectPath,
+            #         self.config.archivePath,
+            #         self.config.exportArchivePath,
+            #         self.config.exportOptionsPlistFilePath,
+            #         self.config.identity,
+            #         self.config.profile
+            #     ))
+
+            print('cd %s; xcodebuild -exportArchive -archivePath %s  -exportPath %s -exportOptionsPlist %s CODE_SIGN_IDENTITY=%s PROVISIONING_PROFILE=%s' % (
+                    self.config.projectPath,
+                    self.config.archivePath,
+                    self.config.exportArchivePath,
+                    self.config.exportOptionsPlistFilePath,
+                    self.config.identity,
+                    self.config.profile
+                ))
+        else:
+            os.system('cd %s;xcodebuild -target %s clean' % (self.config.projectPath, self.config.targetName))
+        return
+
+
 #主函数
 def main():
     #设置配置文件路径
-    initConfigFile()
+    if initConfigFile():
+        sys.exit(-1)
+
+    tool = ipaTool()
+    tool.beginWork()
+
 
 
 main()
--- FYF_ipa_config.py
+++ FYF_ipa_config.py
@@ -6,34 +6,55 @@ import sys
 import inspect
 
 # commandPath = '.'
-commandPath = '/Users/xuekai/.ipa_build_py'
-# 配置文件名
-configFileName = "ipaBuildPyConfigFile.json"
-# 配置文件路径
-configFilePath = os.path.join(commandPath, configFileName)
+# commandPath = '/Users/xuekai/.ipa_build_py'
+# # 配置文件名
+# configFileName = "ipaBuildPyConfigFile.json"
+# # 配置文件路径
+# configFilePath = os.path.join(commandPath, configFileName)
 
-class ipaConfig(object):
+class IpaConfig(object):
     targetName = None
-    gitPath = None
+    # 工程目录
+    projectPath = None
+    # archive 目录
+    archivePath = None
+    # 编译环境
+    # CONFIGURATION = "Release"
+    # #Release 环境  Debug 环境
+    configuration = None
+    # export ipa 目录
+    exportOptionsPlist = None
+    exportArchivePath = None
+
+
+
+    identity = None
+    profile = None
+
     certificateName = None
+    keychainPassword = None
+
+    tempFinder = None
+
+    gitPath = None
+
     firToken = None
+
     emailFromUser = None
     emailToUser = None
     emailPassword = None
     emailHost = None
-    tempFinder = None
-    mainPath = None
-    keychainPassword = None
 
-    def __init__(self):
-        self.createConfigFile()
 
-    def createConfigFile(self):
-        if not os.path.exists(commandPath):
-            try:
-                os.makedirs(commandPath)
-            except OSError:
-                print('create commandPath error')
+    # def __init__(self):
+    #     self.createConfigFile()
+
+    # def createConfigFile(self):
+    #     if not os.path.exists(commandPath):
+    #         try:
+    #             os.makedirs(commandPath)
+    #         except OSError:
+    #             print('create commandPath error')
 
     # 初始化json配置文件
     def initConfigFileContent(self):
@@ -50,18 +71,18 @@ class ipaConfig(object):
 # 另一种序列化的方法
 # print(json.dumps(s, default=lambda obj: obj.__dict__))
 
-def serialize_instance(obj):
-    d = { '__classname__' : type(obj).__name__ }
-    d.update(vars(obj))
-    return d
-
-def unserialize_object(d):
-    clsname = d.pop('__classname__', None)
-    if clsname:
-        cls = classes[clsname]
-        obj = cls.__new__(cls) # Make instance without calling __init__
-        for key, value in d.items():
-            setattr(obj, key, value)
-        return obj
-    else:
-        return d
+# def serialize_instance(obj):
+#     d = { '__classname__' : type(obj).__name__ }
+#     d.update(vars(obj))
+#     return d
+#
+# def unserialize_object(d):
+#     clsname = d.pop('__classname__', None)
+#     if clsname:
+#         cls = classes[clsname]
+#         obj = cls.__new__(cls) # Make instance without calling __init__
+#         for key, value in d.items():
+#             setattr(obj, key, value)
+#         return obj
+#     else:
+#         return d
